66c9a8c98ec5e68487fa622600ccfbb572ef6d45
David Shams <david.shams@gmail.com>
2019-02-16 19:16:22 -0600

    Unite Testing Check a component has children:
      expect(div.props.children).tobeTruthy()

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,8 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  // const result = ReactDOM.render(<App />, div);
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+  expect(div.props.children).tobeTruthy()
 });
