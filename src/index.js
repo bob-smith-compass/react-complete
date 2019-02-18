ba9dbe7a83804bbe0155eb8caa2dee6bba050777
David Shams <david.shams@gmail.com>
2019-02-17 19:08:40 -0600

    sudo yard add underscore npm i underscore - WILL NOT WORK

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 import PropTypes from 'prop-types';
+import _ from 'underscore';
 
 let model = {
     times: 0
@@ -25,9 +26,22 @@ function Sum(props) {
 // function Sum(props: SumProps) {
 function SumClicker({handleClick}) {
     return(
-        // <button onClick={(e)=> {handleClick('A');}}>{props.a} + {props.b} = { props.a + props.b }</h1>
-        // <button onClick={(e)=> {handleClick('A');}}>Pass data as props</button>
-        <button onClick={(e)=> {handleClick(e);}}>Pass data as props</button>
+        <div>
+            <button onClick={(e)=> {handleClick('A');}}>A</button>
+            <button onClick={(e)=> {handleClick('B');}}>Pass data as props: B</button>
+            <button onClick={(e)=> {handleClick(e);}}>Pass data as props</button>
+        </div>
+    )
+}
+function TicTacTo(numOfSquares, onSelection) {
+    // sudo yard add underscore
+    const makeSquares = v => <button key={v} id={v} onClick={event => {
+        onSelection(event.target.id)
+    }}>{v}</button>;
+    return(
+        <div>
+            {_.range(1, numOfSquares + 1).map(makeSquares)}
+        </div>
     )
 }
 Sum.propTypes = {
@@ -61,7 +75,7 @@ function render() {
     // ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
     // ReactDOM.render(<Sum  a={2} b={3}></Sum>, document.getElementById('root'));
     // ReactDOM.render(<Sum  {...props}></Sum>, document.getElementById('root'));
-    ReactDOM.render(<SumClicker handleClick={(l)=> console.log(l)}></SumClicker>, document.getElementById('root'));
+    ReactDOM.render(<SumClicker handleClick={(e)=> {console.log(e); console.log(e.pageX, e.pageY)}}></SumClicker>, document.getElementById('root'));
     // ReactDOM.render(<Person a={2} b={3}></Person>, document.getElementById('root'));
     // ReactDOM.render(<ClickCounter a={2} b={3}></ClickCounter>, document.getElementById('root'));
 }
