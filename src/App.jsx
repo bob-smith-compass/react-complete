928dd8584b335958847944347eafef7da9424f88
David Shams <david.shams@gmail.com>
2019-02-21 12:39:24 -0600

    fetch github user name in React

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,124 @@ import './App.css';
 import Login from './login/Login';
 // import Route from 'react-router-dom';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           {/* <img src={logo} className="App-logo" alt="logo" /> */}
+//             <div onClick={this.props.onClick}>This div cliked {this.props.times} times!</div>
+//             <Login 
+//             username="jdoe"
+//             password="pass"
+//             now={new Date().toISOString()} />
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//         {/* <Route exact path = '/about' ></Route> */}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
 class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      repos: []
+    };
+  }
+  
+  handleSearch = (user) =>{
+    let url = 'https://api.github.com/users/'+user+'/repos';
+ fetch(url).
+  then(response => response.json()).then((repos) => {
+      console.log(repos);
+      console.log(repos.length);
+      this.setState({
+        repos: repos
+      });
+    });
+  };
+  
+  render(){
+    return (
+      <div className="app-container">
+        <h3>React fetch example</h3>
+        <SearchBar handleSubmit={this.handleSearch} />
+        <RepoList repos={this.state.repos}/>
+      </div>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+    
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const text = event.target.text.value;
+    this.props.handleSubmit(text);
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-            <div onClick={this.props.onClick}>This div cliked {this.props.times} times!</div>
-            <Login 
-            username="jdoe"
-            password="pass"
-            now={new Date().toISOString()} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        {/* <Route exact path = '/about' ></Route> */}
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="text"
+          className="form-control"
+          type="text"
+          placeholder="Type github user and press ENTER"
+        />
+      </form>
     );
   }
 }
 
+
+class RepoList extends React.Component {
+
+  render(){
+    var rows = [];
+      this.props.repos.map((repo,index) => rows.push(<RepoItem key={index} repo={repo} />))
+    return (
+      <div className="list-group">
+        {rows}
+      </div>
+    )
+  }
+}
+RepoList.defaultProps = {
+  repos: []
+};
+
+class RepoItem extends React.Component {
+  render(){
+    return (
+        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+    <div className="d-flex w-100 justify-content-between">
+      <h5 className="mb-1">{this.props.repo.name}</h5>
+      <small>{new Date(Date.parse(this.props.repo.created_at)).toLocaleDateString()}</small>
+    </div>
+    <p className="mb-1">{this.props.repo.description}</p>
+    <small>{this.props.repo.html_url}</small>
+  </a>
+    )
+  }
+}
+
 export default App;
+
+// ReactDOM.render(<App/>,document.getElementById('app'));
--- src/index.js
+++ src/index.js
@@ -202,7 +202,7 @@ const view = (model) => {
     return(
         <div>
             <div>{minutes}:{seconds}</div>
-            <button onClick={handleClic}>{model.running? 'Stop': 'Start'}</button>
+            <button onClick={handleClic}>{model.running? 'Start' :'Stop'}</button>
         </div>
     )
 };
@@ -213,7 +213,148 @@ let intents = {
     STOP: 'STOP',
     RESET: 'RESET'
 }
+/**
+ * Redux
+ */
+// let container = Redux.createStore(model);
+
 const props = { a: 2, b: 3 };
+
+/**
+ * Fetch data from REST API
+ */
+/* eslint-disable no-undef */
+function search(query, cb) {
+    alert('Fetching...');
+    return fetch(`https://restcountries.eu/rest/v2/name/${query}`, {
+      accept: "application/json"
+    })
+      .then(checkStatus)
+      .then(parseJSON)
+      .then(cb);
+  }
+  
+  function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    }
+    const error = new Error(`HTTP Error ${response.statusText}`);
+    error.status = response.statusText;
+    error.response = response;
+    console.log(error); // eslint-disable-line no-console
+    throw error;
+  }
+  
+  function parseJSON(response) {
+      console.log(response);
+    return response.json();
+  }
+  
+  const Client = { search };
+  export default Client;
+ // end fetch data
+ Client.search('canada', console.log(response));
+
+/**
+ * Full example fetch
+ */
+/*
+class App extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      repos: []
+    };
+  }
+  
+  handleSearch = (user) =>{
+    let url = 'https://api.github.com/users/'+user+'/repos';
+ fetch(url).
+  then(response => response.json()).then((repos) => {
+      console.log(repos);
+      console.log(repos.length);
+      this.setState({
+        repos: repos
+      });
+    });
+  };
+  
+  render(){
+    return (
+      <div className="app-container">
+        <h3>React fetch example</h3>
+        <SearchBar handleSubmit={this.handleSearch} />
+        <RepoList repos={this.state.repos}/>
+      </div>
+    )
+  }
+}
+
+class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+    
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const text = event.target.text.value;
+    this.props.handleSubmit(text);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          name="text"
+          className="form-control"
+          type="text"
+          placeholder="Type github user and press ENTER"
+        />
+      </form>
+    );
+  }
+}
+
+
+class RepoList extends React.Component {
+
+  render(){
+    var rows = [];
+      this.props.repos.map((repo,index) => rows.push(<RepoItem key={index} repo={repo} />))
+    return (
+      <div className="list-group">
+        {rows}
+      </div>
+    )
+  }
+}
+RepoList.defaultProps = {
+  repos: []
+};
+
+class RepoItem extends React.Component {
+  render(){
+    return (
+        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+    <div className="d-flex w-100 justify-content-between">
+      <h5 className="mb-1">{this.props.repo.name}</h5>
+      <small>{new Date(Date.parse(this.props.repo.created_at)).toLocaleDateString()}</small>
+    </div>
+    <p className="mb-1">{this.props.repo.description}</p>
+    <small>{this.props.repo.html_url}</small>
+  </a>
+    )
+  }
+}
+
+ReactDOM.render(<App/>,document.getElementById('app'));
+*/
+ // end example
+
+
+
+
 function render() {
     // ReactDOM.render(<App onClick={()=> {model.times += 1; console.log('Clicked!'); render(); }} times={model.times}/>, document.getElementById('root'));
     // ReactDOM.render(<div>Hello</div>, document.getElementById('root'));
@@ -222,7 +363,12 @@ function render() {
     // ReactDOM.render(<SumClicker handleClick={(e)=> {console.log(e); console.log(e.pageX, e.pageY)}}></SumClicker>, document.getElementById('root'));
     // ReactDOM.render(<Events ></Events>, document.getElementById('root'));
     // ReactDOM.render(<ECompoment ></ECompoment>, document.getElementById('root'));
-    ReactDOM.render(view(model), document.getElementById('root'));
+    // ReactDOM.render(view(model), document.getElementById('root'));
+    /**
+     * fetch
+     */
+    ReactDOM.render(<App/>, document.getElementById('root'));
+
     // ReactDOM.render(<BroswerRouter> <ECompoment ></ECompoment></BrowserRouter>, document.getElementById('root'));
     // ReactDOM.render(<TicTacTo numOfSquares={6} onSelection={(e)=> {console.log(e); console.log(e.pageX, e.pageY)}}></TicTacTo>, document.getElementById('root'));
     // ReactDOM.render(<Person a={2} b={3}></Person>, document.getElementById('root'));
